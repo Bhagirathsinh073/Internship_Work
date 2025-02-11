6ac9444ac5547a5111fd2af6be6f33ad66463cdb
Bhagirathsinnh073 <bhagirathsinh.parshtechnologies@gmail.com>
2025-02-11 16:03:40 +0530

    Done with Template literate with task
    using Template Literate and Reduce Method with Ternary operator.

--- Lesson-1/Template_literate.js
+++ Lesson-1/Template_literate.js
@@ -3,3 +3,19 @@ let b = "world";
 
 
 let word = `first Word ${a} and second Word ${b}`;
+
+//TASK FOR TEMPLATE_LITERATE
+const product = [
+    {name: "first",price:"200"},
+    {name: "second",price:"400"},
+    {name: "Third",price:"600"},
+    ]
+
+
+    const summary = `The most expensive product is ${
+        product.reduce((max, product) => product.price > max.price ? product : max).name
+    }, priced at ${
+        product.reduce((max, product) => product.price > max.price ? product : max).price
+    }.`;
+
+    console.log(summary);
