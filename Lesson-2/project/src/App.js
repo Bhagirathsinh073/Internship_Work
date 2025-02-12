6b3d3a57544b2851dfa357e9e6b617677a484487
Bhagirathsinnh073 <bhagirathsinh.parshtechnologies@gmail.com>
2025-02-12 15:42:23 +0530

    Setup React Rounter
    React Rounter package install and setup in project and create simple Navbar which navigate to Different page

--- Lesson-2/project/src/App.js
+++ Lesson-2/project/src/App.js
@@ -1,13 +1,29 @@
-// import logo from './logo.svg';
 import './App.css';
 // import AxiosCoM from './Componts/AxiosCom';
-import AxiosPost from './Componts/AxiosPost';
-
+// import AxiosPost from './Componts/AxiosPost';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './Pages/Home'
+import Contact from './Pages/Contact';
+import Layout from './Pages/Layout';
+import Blog from './Pages/Blog';
+import NavBar from './Componts/NavBar';
+import Nothing from './Pages/Nothing';
 function App() {
   return (
     <>
+    <BrowserRouter>
+    <NavBar/>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/blog' element={<Blog/>}/>
+          <Route path='/layout' element={<Layout/>}/>
+          <Route path='*' element={<Nothing/>}/>
+      </Routes>
+    
+    </BrowserRouter>
     {/* <AxiosCoM/> */}
-    <AxiosPost/>
+    {/* <AxiosPost/> */}
     </>
   );
 }
