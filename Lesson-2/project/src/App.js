d2f12cfa0099929cf4b2c1b86361edf0ea0fc47f
Bhagirathsinnh073 <bhagirathsinh.parshtechnologies@gmail.com>
2025-02-13 13:57:05 +0530

    useEfferct click event

--- Lesson-2/project/src/App.js
+++ Lesson-2/project/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import './App.css';
 // import AxiosCoM from './Componts/AxiosCom';
 // import AxiosPost from './Componts/AxiosPost';
@@ -6,7 +7,8 @@ import { BrowserRouter} from 'react-router-dom';
 // import LifeCycle from './Componts/LifeCycle';
 import UseState from './Hooks/UseState';
 import Useeffect from './Hooks/Useeffect';
-import RouterB from './Componts/RouterB';
+import UseContext_Main from './Hooks/UseContext/Usecontext_Main';
+// import RouterB from './Componts/RouterB';
 function App() {
   return (
     <>
@@ -15,8 +17,9 @@ function App() {
     {/* <LifeCycle/> ' Life cycle of react */}
     <UseState/> 
     <Useeffect/>
+    <UseContext_Main/>
     <BrowserRouter>
-    <RouterB/> 
+    {/* <RouterB/>  */}
     </BrowserRouter>
     {/* <AxiosCoM/> */}
     {/* <AxiosPost/> */}
