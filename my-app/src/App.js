4705d52b10c0a865131a4334c7f6e861b53fb075
Bhagirathsinh073 <bhagirathsinh.parshtechnologies@gmail.com>
2025-02-21 16:29:01 +0530

    Jest Library- overview

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,22 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 // import TestTemp from './Task/Temp';
-import Test1 from './Task/Test1';
+// import Test1 from './Task/Test1';
+import Button from './Button/Button'
+import Text from './Text/Text';
 
 function App() {
+  const [toggle, setToggle] = useState(false);
   return (
-      <Test1/>  
-      // <TestTemp/>  
+      // <Test1/>  
+      // <TestTemp/>
+      <>
+     
+    {/* <Text toggle={toggle} displayTxt="Hello World" /> */}
+    {/* <Button setToggle={setToggle} btnTxt="Toggle Text" /> */}
+    
+      </>
   );
 }
 
